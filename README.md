ba383d6c875d6272a52e8068dd57478609e3aac2
jeongnna <lulumin13@gmail.com>
2019-01-22 15:51:08 +0900

    Update

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS231_assignments
-We will share assignments for CS231 in this repository.
--- cs231n_hw1/onehot_encoder.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def onehot_encoder(array, num_label):
-    length = len(array)
-    res = np.zeros((length, num_label))
-    res[np.arange(length), array] = 1
-    return res
-
